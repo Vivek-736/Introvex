9d35f33ff59dd7a38f2b2b534ec53f475c4ae2ea
Vivek <vivekousik8555@gmail.com>
2025-05-28 13:39:12 +0530

    minuscule UI tweak in clerk UI component

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      appearance={{
+        layout: {
+          unsafe_disableDevelopmentModeWarnings: true,
+        },
+      }}
+    >
       <html lang="en" className="scroll-smooth" suppressHydrationWarning>
         <body className={`${poppins.variable} font-sans antialiased`}>
           <ThemeProvider
