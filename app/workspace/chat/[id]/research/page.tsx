186b3345db0067cb83d8b5361ec893c22e46d0a0
Vivek <vivekousik8555@gmail.com>
2025-06-29 12:27:45 +0530

    added a custom loader to the research page transiently

---
+++ app/workspace/chat/[id]/research/page.tsx
@@ -0,0 +1,12 @@
+import ResearchLoader from '@/components/ResearchLoader'
+import React from 'react'
+
+const ResearchPage = () => {
+    return (
+        <div>
+            <ResearchLoader />
+        </div>
+    )
+}
+
+export default ResearchPage
